9901968f21e65016c90b52c2ce25bcf0c84f0bb0
alex <467146659@qq.com>
2018-06-29 18:03:30 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"./logger"
+
+)
+
+const (
+	requestHead = "http://jandan.net/ooxx"
+	requestPageHeadPrefix = "http://jandan.net/ooxx/page-"
+	requestPageHeadSuffix = "#comments"
+	path = "./meizhi"
+	concurrent = 3 //并发
+)
+
+
+func getPageNum() int {
+	//TODO
+	return 0
+}
+
+
+func getPicUrl() string{
+	//TODO
+	return ""
+}
+
+
+func download() {
+	//TODO
+}
+
+func main(){
+	//TODO
+	fmt.Print("hello")
+	logger.Info("hello")
+}
